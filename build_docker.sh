bbaa7554d70ca21097f4dd231c634648509f0870
lachlan <lachlan.00@gmail.com>
2022-04-30 13:18:37 +1000

    clone into the docker dir

--- build_docker.sh
+++ build_docker.sh
@@ -17,11 +17,11 @@ fi
 # MASTER
 if [ $BRANCH = "master" ] || [ $BRANCH = "all" ]; then
   if [ ! -d $AMPACHEDIR/docker/ampache-docker/ ]; then
-    git clone -b master https://github.com/ampache/ampache-docker.git ampache-docker
+    cd $AMPACHEDIR/docker && git clone -b master https://github.com/ampache/ampache-docker.git ampache-docker
   fi
   if [ ! -f $AMPACHEDIR/docker/ampache-docker/Dockerfile ]; then
     rm -rf $AMPACHEDIR/docker/ampache-docker
-    git clone -b master https://github.com/ampache/ampache-docker.git ampache-docker
+    cd $AMPACHEDIR/docker && git clone -b master https://github.com/ampache/ampache-docker.git ampache-docker
   fi
   cd $AMPACHEDIR/docker/ampache-docker/ && git reset --hard origin/master && git pull && nohup docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${a_version} -t ampache/ampache:5 -t ampache/ampache:${a_version} -t ampache/ampache:latest --push . &
 fi
@@ -29,11 +29,11 @@ fi
 # NOSQL
 if [ $BRANCH = "nosql" ] || [ $BRANCH = "all" ]; then
   if [ ! -d $AMPACHEDIR/docker/ampache-docker-nosql/ ]; then
-    git clone -b nosql https://github.com/ampache/ampache-docker.git ampache-docker-nosql
+    cd $AMPACHEDIR/docker && git clone -b nosql https://github.com/ampache/ampache-docker.git ampache-docker-nosql
   fi
   if [ ! -f $AMPACHEDIR/docker/ampache-docker-nosql/Dockerfile ]; then
     rm -rf $AMPACHEDIR/docker/ampache-docker-nosql
-    git clone -b nosql https://github.com/ampache/ampache-docker.git ampache-docker-nosql
+    cd $AMPACHEDIR/docker && git clone -b nosql https://github.com/ampache/ampache-docker.git ampache-docker-nosql
   fi
   cd $AMPACHEDIR/docker/ampache-docker-nosql/ && git reset --hard origin/nosql && git pull && nohup docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${a_version} -t ampache/ampache:nosql5 -t ampache/ampache:nosql${a_version} -t ampache/ampache:nosql --push . &
 fi
@@ -41,11 +41,11 @@ fi
 # DEVELOP
 if [ $BRANCH = "develop" ] || [ $BRANCH = "all" ]; then
   if [ ! -d $AMPACHEDIR/docker/ampache-docker-develop/ ]; then
-    git clone -b develop https://github.com/ampache/ampache-docker.git ampache-docker-develop
+    cd $AMPACHEDIR/docker && git clone -b develop https://github.com/ampache/ampache-docker.git ampache-docker-develop
   fi
   if [ ! -f $AMPACHEDIR/docker/ampache-docker-develop/Dockerfile ]; then
     rm -rf $AMPACHEDIR/docker/ampache-docker-develop
-    git clone -b develop https://github.com/ampache/ampache-docker.git ampache-docker-develop
+    cd $AMPACHEDIR/docker && git clone -b develop https://github.com/ampache/ampache-docker.git ampache-docker-develop
   fi
   cd $AMPACHEDIR/docker/ampache-docker-develop/ && git reset --hard origin/develop && git pull && nohup docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 -t ampache/ampache:develop -t ampache/ampache:preview --push . &
 fi
