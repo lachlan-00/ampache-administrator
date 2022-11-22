5c5c414c2dea7ecaebb2f91943ce16568576f376
lachlan <lachlan.00@gmail.com>
2022-11-22 16:10:53 +1000

    Update build_release.sh
    remove old vendor folder for squashed zips

--- build_release.sh
+++ build_release.sh
@@ -81,7 +81,7 @@ cp -rf $AMPACHEDIR/extras/prettyphoto/* ./public/lib/components/prettyphoto
 find . -name "*.map.1" -exec rm {} \;
 
 cd $AMPACHEDIR/php74_squashed
-rm -rf ./composer.lock vendor/* public/lib/components/* ./docker/ && php7.4 $COMPOSERPATH install
+rm -rf ./composer.lock vendor/* ./lib/components/* ./docker/ && php7.4 $COMPOSERPATH install
 php7.4 $COMPOSERPATH install
 find . -xtype l -exec rm {} \;
 cp $AMPACHEDIR/extras/jquery.contextMenu.min.js.map ./lib/components/jQuery-contextMenu/dist/
@@ -100,7 +100,7 @@ cp -rf $AMPACHEDIR/extras/prettyphoto/* ./public/lib/components/prettyphoto
 find . -name "*.map.1" -exec rm {} \;
 
 cd $AMPACHEDIR/php80_squashed
-rm -rf ./composer.lock vendor/* public/lib/components/* ./docker/ && php8.0 $COMPOSERPATH install
+rm -rf ./composer.lock vendor/* ./ib/components/* ./docker/ && php8.0 $COMPOSERPATH install
 php8.0 $COMPOSERPATH install
 find . -xtype l -exec rm {} \;
 cp $AMPACHEDIR/extras/jquery.contextMenu.min.js.map ./lib/components/jQuery-contextMenu/dist/
@@ -119,7 +119,7 @@ cp -rf $AMPACHEDIR/extras/prettyphoto/* ./public/lib/components/prettyphoto
 find . -name "*.map.1" -exec rm {} \;
 
 cd $AMPACHEDIR/php81_squashed
-rm -rf ./composer.lock vendor/* public/lib/components/* ./docker/ && php8.1 $COMPOSERPATH install
+rm -rf ./composer.lock vendor/* ./lib/components/* ./docker/ && php8.1 $COMPOSERPATH install
 php8.1 $COMPOSERPATH install
 find . -xtype l -exec rm {} \;
 cp $AMPACHEDIR/extras/jquery.contextMenu.min.js.map ./lib/components/jQuery-contextMenu/dist/
