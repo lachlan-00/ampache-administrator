eda49ce1a5ede0de1d7978eaae112f570bf126e2
lachlan <lachlan.00@gmail.com>
2024-05-18 10:19:58 +1000

    dont allow preview from docker6 build

--- build_docker6.sh
+++ build_docker6.sh
@@ -56,16 +56,7 @@ if [ $BRANCH = "develop" ] || [ $BRANCH = "all" ]; then
 fi
 
 # PREVIEW
-if [ $BRANCH = "preview" ] || [ $BRANCH = "all" ]; then
-  if [ ! -d $AMPACHEDIR/docker/ampache-docker-preview/ ]; then
-    cd $AMPACHEDIR/docker && git clone -b preview https://github.com/ampache/ampache-docker.git ampache-docker-preview
-  fi
-  if [ ! -f $AMPACHEDIR/docker/ampache-docker-preview/Dockerfile ]; then
-    rm -rf $AMPACHEDIR/docker/ampache-docker-preview
-    cd $AMPACHEDIR/docker && git clone -b preview https://github.com/ampache/ampache-docker.git ampache-docker-preview
-  fi
-  cd $AMPACHEDIR/docker/ampache-docker-preview/ && git checkout preview && git reset --hard origin/preview && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 -t ampache/ampache:preview --push . &
-fi
+# @see build_docker7.sh
 
 # go home
 cd $AMPACHEDIR
--- build_docker7.sh
+++ build_docker7.sh
@@ -76,7 +76,6 @@ if [ $BRANCH = "preview" ]; then
     cd $AMPACHEDIR/docker && git clone -b preview https://github.com/ampache/ampache-docker.git ampache-docker-nosql-preview
   fi
   cd $AMPACHEDIR/docker/ampache-docker-nosql-preview/ && git checkout preview && git reset --hard origin/preview && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 -t ampache/ampache:nosql-preview --push . &
-
 fi
 
 # go home
