fe29cce5b296a5d6175e4e99a432cc59c67c0446
lachlan <lachlan.00@gmail.com>
2024-10-23 15:14:22 +1000

    Update build_docker6.sh
    remove latest and nosql tags from ampache6

--- build_docker6.sh
+++ build_docker6.sh
@@ -28,7 +28,7 @@ if [ $BRANCH = "master" ] || [ $BRANCH = "all" ]; then
     rm -rf $AMPACHEDIR/docker/ampache-docker
     cd $AMPACHEDIR/docker && git clone -b master https://github.com/ampache/ampache-docker.git ampache-docker
   fi
-  cd $AMPACHEDIR/docker/ampache-docker/ && git checkout master && git reset --hard origin/master && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${RELEASEVERSION} -t ampache/ampache:6 -t ampache/ampache:${RELEASEVERSION} -t ampache/ampache:latest --push . &
+  cd $AMPACHEDIR/docker/ampache-docker/ && git checkout master && git reset --hard origin/master && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${RELEASEVERSION} -t ampache/ampache:6 -t ampache/ampache:${RELEASEVERSION} --push . &
 fi
 
 # NOSQL
@@ -40,7 +40,7 @@ if [ $BRANCH = "nosql" ] || [ $BRANCH = "all" ]; then
     rm -rf $AMPACHEDIR/docker/ampache-docker-nosql
     cd $AMPACHEDIR/docker && git clone -b nosql https://github.com/ampache/ampache-docker.git ampache-docker-nosql
   fi
-  cd $AMPACHEDIR/docker/ampache-docker-nosql/ && git checkout nosql && git reset --hard origin/nosql && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${RELEASEVERSION} -t ampache/ampache:nosql6 -t ampache/ampache:nosql${RELEASEVERSION} -t ampache/ampache:nosql --push . &
+  cd $AMPACHEDIR/docker/ampache-docker-nosql/ && git checkout nosql && git reset --hard origin/nosql && git pull && docker buildx build --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg VERSION=${RELEASEVERSION} -t ampache/ampache:nosql6 -t ampache/ampache:nosql${RELEASEVERSION} --push . &
 fi
 
 # DEVELOP
