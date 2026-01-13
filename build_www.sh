a3ad22aba128d5957eaa240d5fbdee57e89bb880
lachlan <lachlan.00@gmail.com>
2026-01-13 16:50:33 +1000

    Update build_www.sh

--- build_www.sh
+++ build_www.sh
@@ -21,10 +21,16 @@ if [ ! -d $AMPACHEDIR/www ]; then
   mkdir $AMPACHEDIR/www
 fi
 if [ ! -d $AMPACHEDIR/www/ampache.org-api ]; then
-  git clone https://github.com/ampache/ampache.org-api.git www/ampache.org-api
+  cd $AMPACHEDIR/www && git clone https://github.com/ampache/ampache.org-api.git ampache.org-api
 fi
 if [ ! -d $AMPACHEDIR/www/ampache.org-api ]; then
-  cd $AMPACHEDIR/www && git clone -b master https://github.com/ampache/ampache.github.io.git ampache.github.io
+  cd $AMPACHEDIR/www && git clone -b master https://github.com/ampache/ampache.org-api.git ampache.org-api
+fi
+if [ ! -d $AMPACHEDIR/www/ampache.org-docs ]; then
+  cd $AMPACHEDIR/www && git clone https://github.com/ampache/ampache.org-docs.git ampache.org-docs
+fi
+if [ ! -d $AMPACHEDIR/www/ampache.org-docs ]; then
+  cd $AMPACHEDIR/www && git clone -b main https://github.com/ampache/ampache.org-docs.git ampache.org-docs
 fi
 if [ ! -f $AMPACHEDIR/www/ampache.github.io/index.html ]; then
   rm -rf ./ampache.github.io
@@ -42,13 +48,17 @@ cd $AMPACHEDIR/ampache-master && git pull
 cd $AMPACHEDIR/ampache-develop && git pull
 cd $AMPACHEDIR/www/ampache.github.io && git pull
 cd $AMPACHEDIR/www/ampache.org-api && git pull
+cd $AMPACHEDIR/www/ampache.org-docs && git pull
 
 # remove the builds each time
 rm -rf $AMPACHEDIR/www/ampache.org-api/build/*
+rm -rf $AMPACHEDIR/www/ampache.org-docs/build/*
 rm -rf $AMPACHEDIR/www/ampache.github.io/api/*
+rm -rf $AMPACHEDIR/www/ampache.github.io/docs/*
 
 # rebuild and copy to the api site
 cd $AMPACHEDIR/www/ampache.org-api && npm run build && cp -rfv ./build/* $AMPACHEDIR/www/ampache.github.io/api/
+cd $AMPACHEDIR/www/ampache.org-docs && npm run build && cp -rfv ./build/* $AMPACHEDIR/www/ampache.github.io/docs/
 
 cp $AMPACHEDIR/www/ampache.org-api/docs/index.md $AMPACHEDIR/ampache-develop/docs/API.md
 cp $AMPACHEDIR/www/ampache.org-api/docs/api-json-methods.md $AMPACHEDIR/ampache-develop/docs/API-JSON-methods.md
