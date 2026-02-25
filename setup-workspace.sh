bf7fd1ad114286b2ea1744de3c806df542561d50
lachlan <lachlan.00@gmail.com>
2026-02-25 19:59:31 +1000

    Update setup-workspace.sh
    cd back to root on update
    missing repos

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -11,25 +11,39 @@ fi
 
 # DEVELOPMENT BRANCHES
 if [ ! -d $AMPACHEDIR/ampache-develop ]; then
-  git clone -b develop https://github.com/ampache/ampache.git ampache-develop
+  cd $AMPACHEDIR && git clone -b develop https://github.com/ampache/ampache.git ampache-develop
 fi
 if [ ! -f $AMPACHEDIR/ampache-develop/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-develop
-  git clone -b develop https://github.com/ampache/ampache.git ampache-develop
+  cd $AMPACHEDIR && git clone -b develop https://github.com/ampache/ampache.git ampache-develop
 fi
 if [ ! -d $AMPACHEDIR/ampache-master ]; then
-  git clone -b master https://github.com/ampache/ampache.git ampache-master
+  cd $AMPACHEDIR && git clone -b master https://github.com/ampache/ampache.git ampache-master
 fi
 if [ ! -f $AMPACHEDIR/ampache-master/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-master
-  git clone -b master https://github.com/ampache/ampache.git ampache-master
+  cd $AMPACHEDIR && git clone -b master https://github.com/ampache/ampache.git ampache-master
 fi
-if [ ! -d $AMPACHEDIR/ampache-squashed ]; then
-  git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed
+if [ ! -d $AMPACHEDIR/ampache-patch7 ]; then
+  cd $AMPACHEDIR && git clone -b patch7 https://github.com/ampache/ampache.git ampache-patch7
 fi
-if [ ! -f $AMPACHEDIR/ampache-squashed/index.php ]; then
-  rm -rf $AMPACHEDIR/ampache-squashed
-  git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed
+if [ ! -f $AMPACHEDIR/ampache-patch7/index.php ]; then
+  rm -rf $AMPACHEDIR/ampache-patch7
+  cd $AMPACHEDIR && git clone -b patch7 https://github.com/ampache/ampache.git ampache-patch7
+fi
+if [ ! -d $AMPACHEDIR/ampache-squashed7 ]; then
+  cd $AMPACHEDIR && git clone -b squashed7 https://github.com/ampache/ampache.git ampache-squashed7
+fi
+if [ ! -f $AMPACHEDIR/ampache-squashed7/index.php ]; then
+  rm -rf $AMPACHEDIR/ampache-squashed7
+  cd $AMPACHEDIR && git clone -b squashed7 https://github.com/ampache/ampache.git ampache-squashed7
+fi
+if [ ! -d $AMPACHEDIR/ampache-client7 ]; then
+  cd $AMPACHEDIR && git clone -b client7 https://github.com/ampache/ampache.git ampache-client7
+fi
+if [ ! -f $AMPACHEDIR/ampache-client7/index.php ]; then
+  rm -rf $AMPACHEDIR/ampache-client7
+  cd $AMPACHEDIR && git clone -b client7 https://github.com/ampache/ampache.git ampache-client7
 fi
 if [ -f $AMPACHEDIR/ampache-develop/composer.lock ]; then
   rm $AMPACHEDIR/ampache-develop/composer.lock
@@ -47,18 +61,18 @@ cd $AMPACHEDIR/ampache-squashed && php $COMPOSERPATH install && cd $AMPACHEDIR
 
 # AMPACHE 5
 if [ ! -d $AMPACHEDIR/ampache-patch5 ]; then
-  git clone -b patch5 https://github.com/ampache/ampache.git ampache-patch5
+  cd $AMPACHEDIR && git clone -b patch5 https://github.com/ampache/ampache.git ampache-patch5
 fi
 if [ ! -f $AMPACHEDIR/ampache-patch5/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-patch5
-  git clone -b patch5 https://github.com/ampache/ampache.git ampache-patch5
+  cd $AMPACHEDIR && git clone -b patch5 https://github.com/ampache/ampache.git ampache-patch5
 fi
 if [ ! -d $AMPACHEDIR/ampache-squashed5 ]; then
-  git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed5
+  cd $AMPACHEDIR && git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed5
 fi
 if [ ! -f $AMPACHEDIR/ampache-squashed5/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-squashed5
-  git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed5
+  cd $AMPACHEDIR && git clone -b squashed https://github.com/ampache/ampache.git ampache-squashed5
 fi
 if [ -f $AMPACHEDIR/ampache-patch5/composer.lock ]; then
   rm $AMPACHEDIR/ampache-patch5/composer.lock
@@ -72,18 +86,18 @@ cd $AMPACHEDIR/ampache-squashed5 && php $COMPOSERPATH install && cd $AMPACHEDIR
 
 # AMPACHE 6
 if [ ! -d $AMPACHEDIR/ampache-patch6 ]; then
-  git clone -b patch6 https://github.com/ampache/ampache.git ampache-patch6
+  cd $AMPACHEDIR && git clone -b patch6 https://github.com/ampache/ampache.git ampache-patch6
 fi
 if [ ! -f $AMPACHEDIR/ampache-patch6/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-patch6
-  git clone -b patch6 https://github.com/ampache/ampache.git ampache-patch6
+  cd $AMPACHEDIR && git clone -b patch6 https://github.com/ampache/ampache.git ampache-patch6
 fi
 if [ ! -d $AMPACHEDIR/ampache-squashed6 ]; then
-  git clone -b squashed6 https://github.com/ampache/ampache.git ampache-squashed6
+  cd $AMPACHEDIR && git clone -b squashed6 https://github.com/ampache/ampache.git ampache-squashed6
 fi
 if [ ! -f $AMPACHEDIR/ampache-squashed6/index.php ]; then
   rm -rf $AMPACHEDIR/ampache-squashed6
-  git clone -b squashed6 https://github.com/ampache/ampache.git ampache-squashed6
+  cd $AMPACHEDIR && git clone -b squashed6 https://github.com/ampache/ampache.git ampache-squashed6
 fi
 
 if [ -f $AMPACHEDIR/ampache-patch6/composer.lock ]; then
